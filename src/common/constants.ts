05e53c95dc6a0853f184b3303fa0aa621d8293d7
solophone <solophone@aliyun.com>
2020-08-08 10:41:10 +0800

    feat: add common status code enum

---
+++ src/common/constants.ts
@@ -0,0 +1,5 @@
+export enum StatusCodeEnum {
+  OK = 'ok', // 处理成功
+  FAIL = 'fail', // 处理失败
+  ERROR = 'error' // 出错，服务异常
+}
--- src/modules/user-module/user.service.ts
+++ src/modules/user-module/user.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { CommonTypes } from '../../types'
 import { User } from './schema/user.schema'
+import { StatusCodeEnum } from '../../common/constants'
+
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
@@ -12,14 +14,14 @@ export class UserService {
       const isExit = await this.userModel.find({ username: user.username })
       // 如果存在，则新增失败
       if (isExit.length > 0) {
-        return { status: 'error', message: '用户名重复', data: '' }
+        return { status: StatusCodeEnum.ERROR, message: '用户名重复', data: '' }
       }
       // 存入数据库，新增成功
       const data = new this.userModel(user)
       await data.save()
-      return { status: 'ok', message: '注册成功', data }
+      return { status: StatusCodeEnum.OK, message: '注册成功', data }
     } catch (err) {
-      return { status: 'error', message: '注册失败', data: err }
+      return { status: StatusCodeEnum.FAIL, message: '注册失败', data: err }
     }
   }
 }
